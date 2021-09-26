9a22bffa1f52837afdd4d66d2e5c0b623d1859dc
josephlee01 <bluewings113@gmail.com>
2021-09-26 18:53:02 +0900

    #5 Project Structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("lib/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["lib/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
